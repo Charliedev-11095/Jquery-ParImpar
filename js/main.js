e067f286d4aac8b2b9a4f37065abedb64c4a593c
CharlX <Carlosroyiga@gmail.com>
2023-07-26 21:31:04 -0600

    yea

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
-    // Agregamos un evento click al botón "Comprobar"
     $('#btnComprobar').on('click', function() {
-        // Obtenemos el valor del número ingresado por el usuario
         var numero = parseInt($('#numero').val());
+        var resultadoElement = $('#resultado'); // Elemento donde se mostrará el resultado
 
-        // Comprobamos si el número es par o impar
-        if (numero % 2 === 0) {
-            // El número es par
-            alert('El número ' + numero + ' es PAR.');
+        if (numero === 0) {
+            resultadoElement.text('El número es cero.');
+        } else if (numero % 2 === 0) {
+            resultadoElement.text('El número ' + numero + ' es PAR.');
         } else {
-            // El número es impar
-            alert('El número ' + numero + ' es IMPAR.');
+            resultadoElement.text('El número ' + numero + ' es IMPAR.');
         }
     });
 });
